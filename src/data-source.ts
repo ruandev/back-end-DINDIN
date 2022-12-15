35b4d99b39d91877bc621a628167849bf711e47a
Ruan Victor <ruan.techno@gmail.com>
2022-12-15 18:38:06 -0300

    change config

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,10 +3,8 @@ import 'reflect-metadata'
 import { DataSource, DataSourceOptions } from 'typeorm'
 import { SeederOptions } from 'typeorm-extension'
 import { MainSeeder } from './seeds/MainSeeder'
-import path from 'path';
 
 const port = process.env.DB_PORT as number | undefined
-const __dirname = path.resolve();
 
 const options: DataSourceOptions & SeederOptions = ({
     type: "postgres",
@@ -15,8 +13,8 @@ const options: DataSourceOptions & SeederOptions = ({
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    entities: [`src/entities/*.{ts,js}`],
-    migrations: [`src/migrations/*.{ts,js}`],
+    entities: [`${__dirname}/entities/*.{ts,js}`],
+    migrations: [`${__dirname}/migrations/*.{ts,js}`],
     seeds: [MainSeeder],
 })
 
